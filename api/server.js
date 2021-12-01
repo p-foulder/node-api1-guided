7084e9ebbe0b2b6f51fe696511922a4bc234a6c7
Dave Kidd <dave@davekidd.net>
2021-11-30 19:23:05 -0700

    wip

--- api/server.js
+++ api/server.js
@@ -53,7 +53,10 @@ server.post("/api/dogs",(req,res)=>{
     }    
 })
 // [PUT] /api/dogs/:id (U of CRUD, update dog with :id using JSON payload)
-
+server.put("/api/dogs/:id",(req,res)=>{
+    const {id} = req.params
+    const changes = req.body
+})
 // [DELETE] /api/dogs/:id (D of CRUD, remove dog with :id)
 
 // [GET] / (Hello World endpoint)
