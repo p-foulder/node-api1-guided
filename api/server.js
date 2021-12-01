6b17f93d79d277e38390784f57377e1b1b00b675
Dave Kidd <dave@davekidd.net>
2021-11-30 19:37:37 -0700

    wip

--- api/server.js
+++ api/server.js
@@ -77,6 +77,7 @@ server.delete("/api/dogs/:id",async (req,res)=>{
     try{
         const {id} = req.params
         const deletedDog = await Dog.delete(id)
+        res.status(201).json(deletedDog)
     }catch(err){
         res.status(500).json({message:err.message})
     }
