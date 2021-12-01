5cdd4657a5ebc1002b62f8ba2cccf019b6f75f3c
Dave Kidd <dave@davekidd.net>
2021-11-30 19:04:23 -0700

    wip

--- api/server.js
+++ api/server.js
@@ -28,11 +28,10 @@ server.get("/api/dogs/:id",(req,res)=>{
     Dog.findById(idVar)
         .then(dog =>{
             if(!dog){
-                
+                res.status(404).json(`Dog ${idVar} does not exist`)
             }else{
                 res.status(200).json(dog)
-            }
-            
+            }            
         })
         .catch(err=>{
             res.status(500).json({message:err.message})
