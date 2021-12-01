3acc77deffc90b6eb6b9448cb3bf4bfa85f259d2
Dave Kidd <dave@davekidd.net>
2021-11-30 19:36:32 -0700

    wip

--- api/server.js
+++ api/server.js
@@ -73,8 +73,12 @@ server.put("/api/dogs/:id", async (req,res)=>{
     }
 })
 // [DELETE] /api/dogs/:id (D of CRUD, remove dog with :id)
-server.delete("/api/dogs/:id",(req,res)=>{
-    
+server.delete("/api/dogs/:id",async (req,res)=>{
+    try{
+
+    }catch(err){
+        res.status(500).json({message:err.message})
+    }
 })
 // [GET] / (Hello World endpoint)
 server.use("*",(req,res)=>{
